03284c3e1b4686a7b33c7cd2e6552cb43c0851a7
Jan Parthemore <janparthemore@gmail.com>
2017-06-10 15:13:49 -0400

    implemented add button to actually add a todo to list

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,16 @@
 /*jshint esversion: 6*/
-const dummyTodo = {
-  task: 'call the dr',
-  isCompleted: false
-};
-const dummyTodo2 = {
-  task: 'call the vet',
-  isCompleted: false
-};
+// const dummyTodo = {
+//   task: 'call the dr',
+//   isCompleted: false
+// };
+// const dummyTodo2 = {
+//   task: 'call the vet',
+//   isCompleted: false
+// };
 
 const TodoApp = {
   rootElement: '#app',
-  myTodos:[dummyTodo,dummyTodo2],
+  myTodos:[],
   start: function(){
     this.cacheDOM();
     this.bindEvents();
@@ -23,23 +23,32 @@ const TodoApp = {
     this.todoList = this.root.querySelector('.todo-list');
   },
   bindEvents: function(){
-    // this.addBtn.addEventListener('click',()=>{
-    //  console.log ('ADD');
-    // });
-    this.addBtn.addEventListener('click',()=>console.log('add'));
+    this.addBtn.addEventListener('click',()=>this.addaTodo());
   },
-
+  addaTodo: function(){
+    const taskVal = this.inputBox.value;
+    if(!taskVal){
+      return;
+    }
+    const toDo ={
+      task: taskVal,
+      isComplete: false
+    };
+    this.myTodos.push(toDo);
+    this.render();
+    this.inputBox.value = '';
+  },
   render: function(){
-    //const lis = this.myTodos.map(todo=>todo.task);
-    //const lis = this.myTodos.map(function(todo,index){return(todo[index])});//not working yet!!
-     const lis = this.myTodos.map(function(todo,index){
-       //return(todo.task);
-       return(`<li>${todo.task}</li>`);
-     }).join('');
-      // const lis = this.myTodos
-      //                //  .map(todo=>'<li>'+todo.task+ '</li>')
-      //                 .map(todo=>`<li>${todo.task}</li>`)
-      //                 .join('');
+    //const lis = this.myTodos.map(todo=>todo.task).join(''); //this works but does not create actual li elements
+
+    //const lis - this.todos.map(function(todo,index){});//structure for code belong - this works!
+      // const lis = this.myTodos.map(function(todo,index){
+      //   //return('<li>' + todo.task + '</li>');//old clunkier syntax
+      //   return(`<li>${todo.task}</li>`);//new ES6 template string
+      // }).join('');
+    const lis = this.myTodos
+                      .map(todo=>`<li>${todo.task}</li>`)
+                      .join('');
      this.todoList.innerHTML = lis;
   }
 
