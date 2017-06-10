11a16bd3e830c27b9c80b539f382579f32343531
Jan Parthemore <janparthemore@gmail.com>
2017-06-10 16:53:50 -0400

    added dynamic delete buttons and functionality to go with them

--- public/js/app.js
+++ public/js/app.js
@@ -31,18 +31,34 @@ const TodoApp = {
     this.render();
     this.inputBox.value = '';
   },
+  cacheDeleteButtons: function(){
+    this.deleteBtns = this.root.querySelectorAll('.delete-button');
+    //console.dir(this.deleteBtns);
+  },
+  bindDeleteEvents: function(){
+      //this.deleteBtns.forEach(()=>{});
+      this.deleteBtns.forEach((btn,index)=>{
+      btn.addEventListener('click',()=>this.deleteaTodo(index));
+    });
+  },
+  deleteaTodo: function(index){
+    this.myTodos.splice(index,1);
+    this.render();
+  },
   render: function(){
     //const lis = this.myTodos.map(todo=>todo.task).join(''); //this works but does not create actual li elements
 
-    //const lis - this.todos.map(function(todo,index){});//structure for code belong - this works!
+    //const lis - this.todos.map(function(todo,index){});//structure for code below - this works!
       // const lis = this.myTodos.map(function(todo,index){
       //   //return('<li>' + todo.task + '</li>');//old clunkier syntax
       //   return(`<li>${todo.task}</li>`);//new ES6 template string
       // }).join('');
     const lis = this.myTodos
-                      .map(todo=>`<li>${todo.task}</li>`)
-                      .join('');
-     this.todoList.innerHTML = lis;
+                    .map(todo=>`<li>${todo.task}<button class='delete-button'>X</button></li>`)
+                    .join('');
+    this.todoList.innerHTML = lis;
+    this.cacheDeleteButtons();
+    this.bindDeleteEvents();
   }
 
 
