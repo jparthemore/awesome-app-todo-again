e4bae3554dbd82a9caf819d55779f879cccdb903
Jan Parthemore <janparthemore@gmail.com>
2017-06-10 19:49:12 -0400

    created separate function to return a single line item (li)

--- public/js/app.js
+++ public/js/app.js
@@ -45,21 +45,27 @@ const TodoApp = {
     this.myTodos.splice(index,1);
     this.render();
   },
+  gimmeLi: function(todo){
+    return (`<li>${todo.task}<button class='delete-button'>X</button></li>`);
+  },
   render: function(){
     //const lis = this.myTodos.map(todo=>todo.task).join(''); //this works but does not create actual li elements
 
-    //const lis - this.todos.map(function(todo,index){});//structure for code below - this works!
-      // const lis = this.myTodos.map(function(todo,index){
+    //const list - this.todos.map(function(todo,index){});//structure for code below - this works!
+      // const list = this.myTodos.map(function(todo,index){
       //   //return('<li>' + todo.task + '</li>');//old clunkier syntax
       //   return(`<li>${todo.task}</li>`);//new ES6 template string
       // }).join('');
+
     const lis = this.myTodos
-                    .map(todo=>`<li>${todo.task}<button class='delete-button'>X</button></li>`)
+                    //.map(todo=>`<li>${todo.task}<button class='delete-button'>X</button></li>`)
+                    .map(todo=>this.gimmeLi(todo))
                     .join('');
     this.todoList.innerHTML = lis;
     this.cacheDeleteButtons();
     this.bindDeleteEvents();
   }
+
 
 
 };
